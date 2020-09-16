e6243f4db5811bada81502111658a6781b8410c1
Haris Hamzić <53975836+hamzic2019@users.noreply.github.com>
2020-09-16 23:52:07 +0200

    LAST

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
+
+
+class Navigation extends Component {
+  render(){
+    return(
+      <div>
+        <h1>NAVIGATION</h1>
+        <hr/>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
--- src/routes/AppRouters.jsx
+++ src/routes/AppRouters.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 
 import Home from './../components/Home'
+import Navigation from './../components/Navigation'
 
 class Routes extends Component {
   render(){
     return(
       <BrowserRouter> 
-        <Route path="/" component={Home} exact={true} />
+        <Navigation />
+        <Switch>
+          <Route path="/" component={Home} exact={true} />
+        </Switch>
       </BrowserRouter>
     );
   }
